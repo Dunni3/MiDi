ae3d1e35d92728bf4e4542e57ae416ff9c90d778
Ian Dunn <ian@iandunn.io>
2024-04-22 10:18:44 -0400

    remove code from collect_dataset_infos.py that is never actually run

--- midi/collect_dataset_infos.py
+++ midi/collect_dataset_infos.py
@@ -5,6 +5,9 @@ import pytorch_lightning as pl
 import os
 import warnings
 import pathlib
+import pickle
+import sys
+from pathlib import Path
 
 import hydra
 import omegaconf
@@ -55,10 +58,7 @@ def main(cfg: omegaconf.DictConfig):
         print("Resuming from {}".format(cfg.general.resume))
         cfg, _ = get_resume(cfg, dataset_infos, train_smiles, cfg.general.resume, test=False)
 
-    # utils.create_folders(cfg)
-    import pickle
-    import sys
-    from pathlib import Path
+    # save the dataset infos object
     cwd = Path.cwd()
     midi_dir = cwd.parent.parent.parent
     dataset_infos_dir = midi_dir / 'model_reconstruction'
@@ -67,65 +67,6 @@ def main(cfg: omegaconf.DictConfig):
     dataset_infos_file = dataset_infos_dir / f'{dataset_config.name}_infos.pkl'
     with open(dataset_infos_file, 'wb') as f:
         pickle.dump(dataset_infos, f)
-    sys.exit()
-
-    model = FullDenoisingDiffusion(cfg=cfg, dataset_infos=dataset_infos, train_smiles=train_smiles)
-
-    callbacks = []
-    # need to ignore metrics because otherwise ddp tries to sync them
-    params_to_ignore = ['module.model.train_smiles', 'module.model.dataset_infos']
-
-    torch.nn.parallel.DistributedDataParallel._set_params_and_buffers_to_ignore_for_model(model, params_to_ignore)
-
-    if cfg.train.save_model:
-        checkpoint_callback = ModelCheckpoint(dirpath=f"checkpoints/{cfg.general.name}",
-                                              filename='{epoch}',
-                                              monitor='val/epoch_NLL',
-                                              save_top_k=5,
-                                              mode='min',
-                                              every_n_epochs=1)
-        # fix a name and keep overwriting
-        last_ckpt_save = ModelCheckpoint(dirpath=f"checkpoints/{cfg.general.name}", filename='last', every_n_epochs=1)
-        callbacks.append(checkpoint_callback)
-        callbacks.append(last_ckpt_save)
-
-    use_gpu = cfg.general.gpus > 0 and torch.cuda.is_available()
-
-    name = cfg.general.name
-    if name == 'debug':
-        print("[WARNING]: Run is called 'debug' -- it will run with fast_dev_run. ")
-
-    trainer = Trainer(gradient_clip_val=cfg.train.clip_grad,
-                      strategy="ddp_find_unused_parameters_true",  # Needed to load old checkpoints
-                      accelerator='gpu' if use_gpu else 'cpu',
-                      devices=cfg.general.gpus if use_gpu else 1,
-                      max_epochs=cfg.train.n_epochs,
-                      check_val_every_n_epoch=cfg.general.check_val_every_n_epochs,
-                      fast_dev_run=cfg.general.name == 'debug',
-                      enable_progress_bar=cfg.train.progress_bar,
-                      callbacks=callbacks,
-                      log_every_n_steps=50 if name != 'debug' else 1,
-                      )
-
-    if not cfg.general.test_only:
-        trainer.fit(model, datamodule=datamodule, ckpt_path=cfg.general.resume)
-        # if cfg.general.name not in ['debug', 'test']:
-        #     trainer.test(model, datamodule=datamodule)
-    else:
-        # Start by evaluating test_only_path
-        for i in range(cfg.general.num_final_sampling):
-            trainer.test(model, datamodule=datamodule, ckpt_path=cfg.general.test_only)
-        if cfg.general.evaluate_all_checkpoints:
-            directory = pathlib.Path(cfg.general.test_only).parents[0]
-            print("Directory:", directory)
-            files_list = os.listdir(directory)
-            for file in files_list:
-                if '.ckpt' in file:
-                    ckpt_path = os.path.join(directory, file)
-                    if ckpt_path == cfg.general.test_only:
-                        continue
-                    print("Loading checkpoint", ckpt_path)
-                    trainer.test(model, datamodule=datamodule, ckpt_path=ckpt_path)
 
 
 if __name__ == '__main__':
